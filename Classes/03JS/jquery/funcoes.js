7c13f8e21ea0dc8e11cf39c1ea5ce5c4a3288d14
Guilherme <guilherme.argomes@gmail.com>
2019-05-08 15:09:06 -0300

    class 08-05

---
+++ Classes/03JS/jquery/funcoes.js
@@ -0,0 +1,47 @@
+// Sintaxe:
+// $(seletor).acao();
+// $(seletor).acao(funtion(){});
+// $(document).ready(function (){});
+
+//document.getElementById("btnCalcular").addEventListener("click",funcao);
+
+// Funcao que encapsula as demais
+$(document).ready(function (){
+  console.log("Documento Carregado.");
+
+  $("p").click(function(){
+    $(this).hide();
+  });
+
+  //Ocultar...
+  $("button[name='ocultar']").click(function (){
+    //$("div[id='texto']").hide();
+    //para propriedade id em particular:
+    $("#texto").hide();
+    $("#links").hide();
+  });
+
+  $("button[name='exibir']").click(function (){
+    //$("div").show();
+    $("#texto").show();
+    $("#links").show();
+  });
+
+  // $("button[name='alternar']").click(function(){});
+  $("#mudar").click(function(){
+    $("#texto").fadeToggle("slow");
+    $("#links").toggle("slow");
+
+  $("p").addClass("text-uppercase");
+  $("p").removeClass("text-danger");
+  $("#titulo").toggleClass("jumbotron");
+  $("button").toggleClass("btn btn-primary");
+
+    if($("#mudar").text() == "Alternar"){
+      $("#mudar").text("Aparecer");
+    }
+    else{
+      $("#mudar").text("Alternar");
+    }
+  });
+});
